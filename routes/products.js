6c762ccf72524bd472a62f074fa107a2ac05b1bc
hsjun <hippo8976@gmail.com>
2025-05-26 16:28:49 +0900

    product list, creat 추가

--- routes/products.js
+++ routes/products.js
@@ -6,13 +6,27 @@ const { db } = require('../utils/db');
 const router = express.Router();
 
 
+router.get('/list', async function(req, res, next) {
+  const products = await db.product.findMany();
+  res.json(products);
+});
 
 router.post('/create', async (req, res, next) => {
   try{
     assert(req.body, CreateDto);
+    const { name, description, price, tags } = req.body;
+    const newProduct = await db.product.create({
+      data: { name, description, price, tags }
+    });
 
-  } catch {}
+    return res.status(201).json({
+      message: '상품이 등록되었습니다.',
+      productId: newProduct.id
+    });
+  } catch (err) {
+     next(err);
+  }
   
-});
+  });
 
 module.exports = router;
