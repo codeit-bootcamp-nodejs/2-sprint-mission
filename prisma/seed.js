6d5d58e560d3f393c2f3b9aa979e46b2c6aacbe9
hsjun <hippo8976@gmail.com>
2025-05-29 15:58:23 +0900

    seed 추가

---
+++ prisma/seed.js
@@ -0,0 +1,46 @@
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+
+async function main() {
+  await prisma.product.createMany({
+    data: [
+      {
+        name: '맥북 프로',
+        description: '중고 모델입니다.',
+        price: 1200000,
+        tags: ['노트북', '애플']
+      },
+      {
+        name: '에어팟 프로',
+        description: '신상 입니다.',
+        price: 700000,
+        tags: ['이어폰', '애플']
+      }
+    ]
+  });
+
+
+  await prisma.article.createMany({
+    data: [
+      {
+        title: '1등',
+        content: '내가 제일 처음으로 글 씀',
+      },
+      {
+        title: '1등 놀이 애도 아니고',
+        content: '그럼 난 2등 ㅋㅋ',
+      }
+    ]
+  });
+};
+
+main()
+  .then( async () => {
+    console.log('seeding 완료');
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error('seeding 실패:', e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
