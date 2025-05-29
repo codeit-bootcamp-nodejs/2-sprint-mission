1d92c59807d9a3dcae767e961bb79dd3146a163f
hsjun <hippo8976@gmail.com>
2025-05-29 11:12:50 +0900

    cors 예시 추가

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors = require('cors');
+
 
 var indexRouter = require('./routes/index');
 var productsRouter = require('./routes/products');
@@ -23,6 +25,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.use(cors({ origin: 'http://localhost:4000' }))
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
