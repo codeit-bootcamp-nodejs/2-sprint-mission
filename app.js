749ec3258eb1580e6b716c43825ac5a3b5069845
hsjun <hippo8976@gmail.com>
2025-05-29 13:56:59 +0900

    코드 정리

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 var createError = require('http-errors');
 var express = require('express');
-var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const cors = require('cors');
+var cors = require('cors');
 
 
 var indexRouter = require('./routes/index');
@@ -13,24 +12,15 @@ var articlecommentsRouter = require('./routes/articlecomments');
 var productcommentsRouter = require('./routes/productcomments');
 var documentsRouter = require('./routes/documents');
 
-
-
-
 var errorHandler = require('./utils/errorHandler');
 
 var app = express();
-
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 app.use(cors({ origin: 'http://localhost:4000' }))
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/products', productsRouter);
@@ -40,26 +30,12 @@ app.use('/productcomments', productcommentsRouter);
 app.use('/documents', documentsRouter);
 
 
+app.use((req, res, next) => {
+  next(createError(404, '요청한 리소스를 찾을 수 없습니다.'));
+});
 
 
 app.use(errorHandler);
 
-
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
 
 module.exports = app;
