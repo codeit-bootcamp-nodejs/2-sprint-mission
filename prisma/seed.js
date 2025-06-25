1ce755404761bb25ad067c503551fb6bf4a544cd
JinsolKim <jinsol032360@gmail.com>
2025-06-25 11:13:41 +0900

    User 시드 추가 작성

--- prisma/seed.js
+++ prisma/seed.js
@@ -2,7 +2,17 @@ const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
 async function main() {
-    // Product 시드
+    // 1. User 시드 생성
+    const user = await prisma.user.create({
+        data: {
+            email: 'jin@example.com',
+            nickname: 'JIN',
+            image: 'https://via.placeholder.com/150',
+            password: 'hashedpassword123', 
+        },
+    });
+
+    // 2. Product 시드
     const product = await prisma.product.create({
         data: {
             name: '노트북',
@@ -10,6 +20,7 @@ async function main() {
             price: 700000,
             tags: ['전자기기', '노트북'],
             imageUrl: 'images/laptop.jpg',
+            authorId: user.id,
         },
     });
 
@@ -17,14 +28,16 @@ async function main() {
         data: {
             content: '좋은 상품이에요!',
             productId: product.id,
+            authorId: user.id,
         },
     });
 
-    // Article 시드
+    // 3. Article 시드
     const article = await prisma.article.create({
         data: {
             title: 'Promise.try',
             content: '동기식 콜백 함수 오류처리가 더 쉬워집니다.',
+            authorId: user.id,
         },
     });
 
@@ -32,6 +45,7 @@ async function main() {
         data: {
             content: '흥미로운 글이네요!',
             articleId: article.id,
+            authorId: user.id,
         },
     });
 
