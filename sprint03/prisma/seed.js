b75a2ac96c0bea38bb6ce7bc61f6d4af02995cfb
hsjun <hippo8976@gmail.com>
2025-06-27 15:44:08 +0900

    feat: seed 데이터 재작성 후 db 적용

--- sprint03/prisma/seed.js
+++ sprint03/prisma/seed.js
@@ -1,40 +1,63 @@
-import { db } from "../src/utils/db.js";
+import { db } from "../src/lib/db.js";
+import { hashPassword } from "../src/utils/hash-password.js";
 
 async function main() {
-  // Product 데이터
-  await db.product.createMany({
-    data: [
-      {
-        name: "맥북 프로",
-        description: "중고 모델입니다.",
-        price: 1200000,
-        stock: 5,
-        tags: ["노트북", "애플"],
-      },
-      {
-        name: "에어팟 프로",
-        description: "신상 입니다.",
-        price: 700000,
-        stock: 10,
-        tags: ["이어폰", "애플"],
-      },
-    ],
-    skipDuplicates: true,
+  const hashedPassword01 = await hashPassword("password123");
+  const hashedPassword02 = await hashPassword("password456");
+
+  const user1 = await db.user.create({
+    data: {
+      email: "test123@test.com",
+      nickname: "tester01",
+      password: hashedPassword01,
+    },
   });
 
-  // Article 데이터
-  await db.article.createMany({
-    data: [
-      {
-        title: "1등",
-        content: "내가 제일 처음으로 글 씀",
-      },
-      {
-        title: "1등 놀이 애도 아니고",
-        content: "그럼 난 2등 ㅋㅋ",
-      },
-    ],
-    skipDuplicates: true,
+  const user2 = await db.user.create({
+    data: {
+      email: "test456@test.com",
+      nickname: "tester02",
+      password: hashedPassword02,
+    },
+  });
+
+  // Product (user1)
+  const product = await db.product.create({
+    data: {
+      name: "맥북 프로",
+      description: "중고 모델입니다.",
+      price: 1200000,
+      stock: 5,
+      tags: ["노트북", "애플"],
+      userId: user1.id,
+    },
+  });
+
+  // 3) ProductComment (user2)
+  const productComment = await db.productComment.create({
+    data: {
+      content: "이거 삽니다",
+      productId: product.id,
+      userId: user2.id,
+    },
+  });
+
+  // 4) Article (user2)
+  const article = await db.article.create({
+    data: {
+      title: "User2 의 첫 글",
+      content: "안녕하세요, User2 입니다.",
+      userId: user2.id,
+    },
+  });
+
+  // 5) ArticleComment (user1)
+  const articleComment = await db.articleComment.create({
+    data: {
+      content: "안녕하세요! 반갑습니다.",
+      articleId: article.id,
+      userId: user1.id,
+    },
   });
 }
 
