2469b07aae3cfb6c0aa558ef95378d76e6366c19
hsjun <hippo8976@gmail.com>
2025-06-27 19:11:21 +0900

    feat: 토큰 재발급 기능 추가

--- sprint03/src/controllers/auth.controller.js
+++ sprint03/src/controllers/auth.controller.js
@@ -1,8 +1,7 @@
 import { db } from "../lib/db.js";
 import { hashPassword } from "../utils/hash.password.js";
 import { comparePassword } from "../utils/compare.password.js";
-
-import { generateTokens } from "../lib/token.js";
+import { generateTokens, verifyRefreshToken } from "../lib/token.js";
 
 // 회원 가입
 async function register(req, res, next) {
@@ -39,13 +38,51 @@ async function login(req, res, next) {
       throw error;
     }
 
-    // 토큰 생성
-    const { accessToken } = generateTokens(user.id);
+    const { accessToken, refreshToken } = generateTokens(user.id);
 
-    res.status(200).json({ message: "로그인 성공", token: accessToken });
+    res.status(200).json({
+      message: "로그인 성공",
+      accesstoken: accessToken,
+      refreshtoken: refreshToken,
+    });
   } catch (err) {
     next(err);
   }
 }
 
-export { register, login };
+async function refreshAccessToken(req, res, next) {
+  try {
+    const { refreshToken } = req.body;
+
+    if (!refreshToken) {
+      const error = new Error("Refresh Token이 없습니다.");
+      error.status = 401;
+      return next(error);
+    }
+
+    const { userId } = verifyRefreshToken(refreshToken);
+
+    const user = await db.user.findUnique({
+      where: { id: userId },
+    });
+
+    if (!user) {
+      const error = new Error("유저를 찾을 수 없습니다.");
+      error.status = 404;
+      return next(error);
+    }
+
+    const { accessToken, refreshToken: newRefreshToken } = generateTokens(
+      user.id
+    );
+
+    res.status(200).json({
+      messege: "토큰 재발급 완료.",
+      accesstoken: accessToken,
+      refreshtoken: newRefreshToken,
+    });
+  } catch (err) {
+    next(err);
+  }
+}
+export { register, login, refreshAccessToken };
--- sprint03/src/routes/auth.js
+++ sprint03/src/routes/auth.js
@@ -1,12 +1,14 @@
 import { Router } from "express";
 import {
-  register, login
+  register,
+  login,
+  refreshAccessToken,
 } from "../controllers/auth.controller.js";
 
 const router = Router();
 
 router.route("/register").post(register);
 router.route("/login").post(login);
-
+router.route("/refresh").post(refreshAccessToken);
 
 export default router;
