9db41c466093655a892332db83f1ccddbf844c69
jinsol kim <jinsol032360@gmail.com>
2025-06-27 19:58:01 +0900

    불필요한 코드 삭제

--- app.js
+++ app.js
@@ -30,55 +30,6 @@ app.use('/article', articleRoutes);
 app.use('/', commentRoutes);
 
 // 파일 정적 서빙
-
-const multer = require('multer');
-
-const { getAllProducts, createProduct, getProductById, updateProduct, deleteProduct } = require('./routes/product');
-const { getAllArticles, createArticle, getArticleById, updateArticle, deleteArticle } = require('./routes/article');
-const {
-    getAllProductComments,
-    getAllArticleComments,
-    createProductComment,
-    createArticleComment,
-    updateProductComment,
-    updateArticleComment,
-    deleteProductComment,
-    deleteArticleComment,
-} = require('./routes/comment');
-const errorHandler = require('./error/errorHendler');
-
-const app = express();
-
-const upload = multer({ dest: 'uploads/' });
-
-// 미들웨어
-app.use(morgan('dev'));
-app.use(cors());
-app.use(express.json());
-
-app.get('/', (req, res) => {
-    res.send('Hello from Render!');
-});
-
-// 라우터
-app.route('/product').get(getAllProducts).post(createProduct);
-
-app.route('/product/:id').get(getProductById).patch(upload.single('file'), updateProduct).delete(deleteProduct);
-
-app.route('/article').get(getAllArticles).post(createArticle);
-
-app.route('/article/:id').get(getArticleById).patch(updateArticle).delete(deleteArticle);
-
-app.route('/product/:productId/comment').get(getAllProductComments).post(createProductComment);
-
-app.route('/product/:productId/comment/:commentId').patch(updateProductComment).delete(deleteProductComment);
-
-app.route('/article/:articleId/comment').get(getAllArticleComments).post(createArticleComment);
-
-app.route('/article/:articleId/comment/:commentId').patch(updateArticleComment).delete(deleteArticleComment);
-
-// localhost:3000/products/files/파일이름 으로 요청하면 uploads/파일이름 파일을 정적으로 서빙
-
 app.use('/product/files', express.static('uploads'));
 
 // 에러 핸들러
@@ -90,4 +41,3 @@ app.listen(PORT, () => {
 });
 
 module.exports = app;
-
