52e957fd8e0cbe639982ceb4cfe61e3719c99106
JinsolKim <jinsol032360@gmail.com>
2025-06-27 10:14:16 +0900

    refreshToken 이용한 토큰 재발급

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,11 +1,11 @@
 const { db } = require('../config/db');
+const { verifyRefreshToken, generateTokens } = require('../lib/token');
 const { registerUserService, loginUserService } = require('../services/auth.service');
-const {setTokenCookies} = require('../utils/cookie.utils')
+const { setTokenCookies } = require('../utils/cookie.utils');
 
 // 회원가입 컨트롤러
 exports.registerUserController = async (req, res, next) => {
     try {
-        console.log('✅ 회원가입 요청 들어옴');
         const { email, nickname, password } = req.body;
         const user = await registerUserService({ email, nickname, password });
 
@@ -42,5 +42,35 @@ exports.loginController = async (req, res, next) => {
         });
     } catch (error) {
         console.error('로그인 ❌', error);
+    }
+};
+
+// 토큰 재발급
+exports.refreshTokenController = async (req, res, next) => {
+    try {
+        const refreshToken = req.cookies['refresh-token'];
+        if (!refreshToken) {
+            return res.status(401).json({ message: 'Refresh Token이 없습니다.' });
+        }
+
+        // 토큰 검증 및 디코딩
+        const { userId } = verifyRefreshToken(refreshToken);
+
+        // DB에서 유저 존재 확인 + refreshToken 비교
+        const user = await db.user.findUnique({ where: { id: userId } });
+        if (!user || user.refreshToken !== refreshToken) {
+            return res.status(403).json({ message: '유효하지 않은 Refresh Token 입니다.' });
+        }
+
+        // 토큰 재발급
+        const { accessToken, refreshToken: newRefreshToken } = generateTokens(user.id);
+        await db.user.update({ where: { id: user.id }, data: { refreshToken: newRefreshToken } });
+
+        setTokenCookies(res, accessToken, newRefreshToken);
+
+        res.status(200).json({ message: 'Access Token이 재발급되었습니다' });
+    } catch (error) {
+        console.error('토큰 재발급 오류 ❌', error);
+        return res.status(401).json({ message: '토큰 재발급 실패' });
     }
 };
--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const passport = require('../lib/passport/index');
-const { registerUserController, loginController } = require('../controllers/auth.controller');
+const { registerUserController, loginController, refreshTokenController } = require('../controllers/auth.controller');
 
 const router = express.Router();
 
@@ -9,5 +9,8 @@ router.post('/register', registerUserController);
 
 // 로그인
 router.post('/login', passport.authenticate('local', { session: false }), loginController);
+
+// 토큰 재발급
+router.post('/refresh', refreshTokenController);
 
 module.exports = router;
