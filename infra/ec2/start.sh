ee3861dcfcd78e67ad3a4c7a27d448d46d40d402
3023kk <3023kk@naver.com>
2025-09-07 20:08:06 +0900

    chore: 주석 수정

--- infra/ec2/start.sh
+++ infra/ec2/start.sh
@@ -3,6 +3,6 @@
 #pm2 설치
 #npm install -g pm2
 
-# ../../build/.env 파일에 있는 환경 변수가 production일 때 사용됨
+# ../../.env 파일에 있는 환경 변수가 production일 때 사용됨
 # src/lib/constants.ts 참조
 pm2 start ecosystem.config.js
