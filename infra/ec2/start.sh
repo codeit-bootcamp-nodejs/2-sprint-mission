6771dbe126ed2a694eef7b2fd3c44535280a42cf
3023kk <3023kk@naver.com>
2025-09-14 01:11:49 +0900

    fix : 명령어 경로 수정

--- infra/ec2/start.sh
+++ infra/ec2/start.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-echo "$HOME"
-
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
@@ -11,4 +9,4 @@ npm install -g pm2
 
 # ../../.env 파일에 있는 환경 변수가 production일 때 사용됨
 # src/lib/constants.ts 참조
-pm2 start ecosystem.config.js
+pm2 start infra/ec2/ecosystem.config.js
--- infra/ec2/stop.sh
+++ infra/ec2/stop.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-echo "$HOME"
-
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
 npm install pm2 -g
-
 pm2 stop server
