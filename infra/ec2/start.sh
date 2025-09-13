7418bcb9f54e87d3a5f18e914610ead59349d030
3023kk <3023kk@naver.com>
2025-09-14 01:04:26 +0900

    fix: script 업데이트

--- infra/ec2/start.sh
+++ infra/ec2/start.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+echo "$HOME"
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
 #pm2 설치
 npm install -g pm2
 
