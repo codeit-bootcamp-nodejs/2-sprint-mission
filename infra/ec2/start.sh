f00042d094e9718e4a0f84498a278c82a5103aeb
3023kk <3023kk@naver.com>
2025-09-14 01:15:53 +0900

    fix : 서비스 실행 스크립트 업데이트

--- infra/ec2/start.sh
+++ infra/ec2/start.sh
@@ -4,6 +4,8 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
+pwd
+
 #pm2 설치
 npm install -g pm2
 
