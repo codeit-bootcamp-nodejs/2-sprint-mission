91bd3e3186a79c0ab2d9cf39c412f28ff4a5110b
3023kk <3023kk@naver.com>
2025-09-14 22:24:10 +0900

    fix: 환경 변수 처리 수정

--- infra/ec2/start.sh
+++ infra/ec2/start.sh
@@ -6,14 +6,10 @@ export NVM_DIR="$HOME/.nvm"
 
 cd $(dirname $0)
 
-# DB 및 AWS 환경 변수 들
-cp "$HOME/config/.env" ../../.env
-
 (cd ../../ && npm install && npx prisma migrate dev)
 
 #pm2 설치
 npm install -g pm2
 
-# ../../.env 파일에 있는 환경 변수가 production일 때 사용됨.
 # src/lib/constants.ts 참조
-pm2 start ecosystem.config.js --node-args="-r dotenv/config"
+pm2 start ecosystem.config.js
